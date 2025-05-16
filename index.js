ebdd298b9984bc26bf04c237ac7cd3adcf81055e
Pavan Prasad <110252432+Pavanmum@users.noreply.github.com>
2025-05-16 15:25:45 +0530

    fix: update CORS configuration to allow requests from the production frontend

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: ['http://localhost:5173'], 
+  origin: ['http://localhost:5173','https://easy-life-frontend-two.vercel.app'], 
   credentials: true,               
 }));
 
